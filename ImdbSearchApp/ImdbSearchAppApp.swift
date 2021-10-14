1a52d8b5e1670d0b2a56de0a56ff6349239195d2
Ali Karababa <karababa.alii@gmail.com>
2021-10-14 16:05:41 +0300

    Initial Commit

---
+++ ImdbSearchApp/ImdbSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImdbSearchAppApp.swift
+//  ImdbSearchApp
+//
+//  Created by Ali Karababa on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ImdbSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
